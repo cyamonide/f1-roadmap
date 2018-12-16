9bdecc1f02f0934d8399222d20d91aaefe0b2eb1
Simon Liu <cyamonide@gmail.com>
2018-12-15 22:30:31 -0500

    Add Node.js server file

---
+++ test.js
@@ -0,0 +1,30 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (req, res) {
+
+	var filePath = req.url;
+	if (filePath == '/')
+		filePath = '/roadmap.html';
+
+	filePath = __dirname+filePath;
+	var extname = path.extname(filePath);
+	var contentType = 'text/html';
+
+	switch (extname) {
+		case '.js':
+		contentType = 'text/javascript';
+		break;
+		case '.css':
+		contentType = 'text/css';
+		break;
+	}
+
+	//Open a file on the server and return its content:
+	fs.readFile(filePath, function(err, data) {
+		res.writeHead(200, {'Content-Type': contentType});
+		res.write(data);
+		return res.end();
+	});
+}).listen(8080);4
