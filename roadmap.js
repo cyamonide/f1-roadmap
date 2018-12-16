25bc8a44250c24413bfcdadb002aa42462ec5e0b
Simon Liu <cyamonide@gmail.com>
2018-12-15 22:29:43 -0500

    Site basics

---
+++ roadmap.js
@@ -0,0 +1,12 @@
+function changeSliderWidth(parent, val) {
+	var paddingLeft = document.getElementById(parent).getElementsByClassName("block-left");
+	var paddingRight = document.getElementById(parent).getElementsByClassName("block-right");
+	
+	var i;
+	for (i = 0; i < paddingLeft.length; i++) {
+    	paddingLeft[i].style.width = val;
+	}
+	for (i = 0; i < paddingRight.length; i++) {
+		paddingRight[i].style.width = val;
+	}
+}
