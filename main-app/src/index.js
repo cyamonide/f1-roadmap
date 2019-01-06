5a5b9709adc9e903c64d7551f7fda54232e3cb15
Simon Liu <cyamonide@gmail.com>
2019-01-06 00:18:10 -0500

    Replace Express app with React

---
+++ main-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Roadmap extends React.Component {
+    render() {
+        return (
+            <h1>This is a thing.</h1>
+        )
+    }
+}
+
+ReactDOM.render(<Roadmap />, document.getElementById('root'));
